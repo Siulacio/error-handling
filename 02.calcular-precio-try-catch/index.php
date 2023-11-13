c5005a94da5cc9caad2602a7cfaaa659f37533b0
Luis Palacio <siulacio@hotmail.com>
2023-11-13 13:57:27 -0500

    lanzando y capturando una excepción personalizada

--- 02.calcular-precio-try-catch/index.php
+++ 02.calcular-precio-try-catch/index.php
@@ -4,7 +4,10 @@ $dividendo = 100;
 $divisor = 0;
 
 try {
+    if ($divisor === 0) {
+        throw new Exception('No se puede dividir por cero');
+    }
     echo $resultado = $dividendo / $divisor;
-} catch (DivisionByZeroError $exception) {
+} catch (Exception $exception) {
     echo $exception->getMessage();
 }
