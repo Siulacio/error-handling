542919d7f6d0725666fa96cc28e696f9149f148b
Luis Palacio <siulacio@hotmail.com>
2023-11-13 20:40:46 -0500

    configurar reporte de errores

---
+++ 03.nivel-visualizacion-errores/index.php
@@ -0,0 +1,12 @@
+<?php
+
+error_reporting(E_ALL | E_STRICT);
+ini_set('display_errors', 1);
+ini_set('log_errors', 1);
+ini_set('error_log', 'php_error.log');
+
+$dividendo = 100;
+$divisor = 0;
+
+$resultado = $dividendo / $divisor;
+
